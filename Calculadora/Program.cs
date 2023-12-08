3513860b10f44c5975b0d99e01f3a04c757b536a
douglasferreirag <douglas.ferreirag@gmail.com>
2023-12-08 15:53:48 -0300

    Método Somar

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculadora.Services;
+
+CalculadoraImpementacao c = new CalculadoraImpementacao();
+
+int numero1 = 5;
+
+int numero2 = 10; 
+
+Console.WriteLine($"{numero1} + {numero2} = {c.Somar(numero1,numero2)}"); 
+
---
+++ Calculadora/Services/CalculadoraImpementacao.cs
@@ -0,0 +1,13 @@
+namespace Calculadora.Services;
+
+public class CalculadoraImpementacao
+{
+
+        public int Somar(int numero1, int numero2){
+
+            return numero1 + numero2 + 1;
+
+        }
+
+
+}
