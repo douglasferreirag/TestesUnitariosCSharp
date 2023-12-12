e60977be1d39940fa03e6282dcca4cc566141fd5
douglasferreirag <douglas.ferreirag@gmail.com>
2023-12-11 21:55:41 -0300

    Esqueleto do Teste

---
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -0,0 +1,27 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+
+    private CalculadoraImpementacao _calc;
+
+    public CalculadoraTestes(){
+
+         _calc = new CalculadoraImpementacao();   
+
+    }
+
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+
+        //Arrange - prepara o cenário
+        //Act - ação
+        //Assert - validação
+
+
+
+    }
+}
